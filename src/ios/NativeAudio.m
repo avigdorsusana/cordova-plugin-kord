bcc60d4c75d28436081846aba23dc878b8a6477a
Avigdor Susana <avigdor@Avigdors-MacBook-Air-2.local>
2019-08-03 15:42:38 -0400

    tunning stuff 13

--- src/ios/NativeAudio.m
+++ src/ios/NativeAudio.m
@@ -84,7 +84,8 @@ NSString* INFO_VOLUME_CHANGED = @"(NATIVE AUDIO) Volume changed.";
     [self.commandDelegate runInBackground:^{
         if (existingReference == nil) {
 
-            NSString* basePath = [[[NSBundle mainBundle] resourcePath] stringByAppendingPathComponent:@"www"];
+            //NSString* basePath = [[[NSBundle mainBundle] resourcePath] stringByAppendingPathComponent:@"www"];
+			NSString* basePath = [[[NSBundle mainBundle] resourcePath] stringByAppendingPathComponent:@"media"];
             NSString* path = [NSString stringWithFormat:@"%@", assetPath];
             NSString* pathFromWWW = [NSString stringWithFormat:@"%@/%@", basePath, assetPath];
 
@@ -113,7 +114,7 @@ NSString* INFO_VOLUME_CHANGED = @"(NATIVE AUDIO) Volume changed.";
             } else {
                 //NSString *RESULT = [NSString stringWithFormat:@"%@ (%@)", ERROR_ASSETPATH_INCORRECT, assetPath];
 				//NSString *RESULT = [NSString stringWithFormat:@"%@ (%@)", ERROR_ASSETPATH_INCORRECT, pathFromWWW]; //Err loading track: (NATIVE AUDIO) Asset not found. (/var/containers/Bundle/Application/5A8792DF-286E-4566-A603-57564E01CD5D/KORD.app/www/http://media.shopy.net/kord/albums/20205P229T4/404Fa4Mx4XP50E404XP50E49.mp3)
-				NSString *RESULT = [NSString stringWithFormat:@"%@ (%@)", ERROR_ASSETPATH_INCORRECT, basePath];
+				NSString *RESULT = [NSString stringWithFormat:@"%@ (%@)", ERROR_ASSETPATH_INCORRECT, basePath]; //[Log] Err loading simple track: (NATIVE AUDIO) Asset not found. (/var/containers/Bundle/Application/8A92D663-A25C-42D0-8868-74DA8F19F052/KORD.app/www) (cordova.js, line 1509, x7)
                 [self.commandDelegate sendPluginResult:[CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString: RESULT] callbackId:callbackId];
             }
         } else {
