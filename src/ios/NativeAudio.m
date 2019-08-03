a81b293093925617ea7e1489a0e732d45d5845d2
Avigdor Susana <avigdor@Avigdors-MacBook-Air-2.local>
2019-08-03 16:46:50 -0400

    tunning stuff

--- src/ios/NativeAudio.m
+++ src/ios/NativeAudio.m
@@ -107,14 +107,14 @@ NSString* INFO_VOLUME_CHANGED = @"(NATIVE AUDIO) Volume changed.";
                 //CFURLRef        soundFileURLRef = (CFURLRef) CFBridgingRetain(pathURL);
                 //SystemSoundID soundID;
                 //AudioServicesCreateSystemSoundID(soundFileURLRef, & soundID);
-				NativeAudioAsset* asset = [[NativeAudioAsset alloc] initWithPath:pathURL
+				/*NativeAudioAsset* asset = [[NativeAudioAsset alloc] initWithPath:pathURL
                                                                       withVoices:1
                                                                       withVolume:1
-                                                                   withFadeDelay:0];
+                                                                   withFadeDelay:0];*/
 				
 
                 //audioMapping[audioID] = [NSNumber numberWithInt:soundID];
-				audioMapping[audioID] = asset;
+				//audioMapping[audioID] = asset;
 
                 NSString *RESULT = [NSString stringWithFormat:@"%@ (%@)", INFO_ASSET_LOADED, audioID];
                 [self.commandDelegate sendPluginResult:[CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString: RESULT] callbackId:callbackId];
