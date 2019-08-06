2d6ed220c882b5f9b54db0b0df1401728ed5df1f
Avigdor Susana <avigdor@Avigdors-MacBook-Air-2.local>
2019-08-06 01:54:34 -0400

    tunning stuff

--- src/ios/NativeAudio.m
+++ src/ios/NativeAudio.m
@@ -143,7 +143,7 @@ NSString* INFO_VOLUME_CHANGED = @"(NATIVE AUDIO) Volume changed.";
 		//[urlData writeToFile:filePath atomically:YES];
 		//success = [urlData writeToFile:filePath atomically:YES];
 		NSError *error;
-		success = [urlData writeToFile:filePath options:0 error:&error];
+		BOOL success = [urlData writeToFile:filePath options:0 error:&error];
 		/*if (!success) {
 			NSLog(@"writeToFile failed with error %@", error);
 		}*/
