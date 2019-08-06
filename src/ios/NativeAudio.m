f5de0eb0435a66a0c450f8b3cebfffab45cc1929
Avigdor Susana <avigdor@Avigdors-MacBook-Air-2.local>
2019-08-06 00:38:35 -0400

    tunning stuff

--- src/ios/NativeAudio.m
+++ src/ios/NativeAudio.m
@@ -201,9 +201,9 @@ NSString* INFO_VOLUME_CHANGED = @"(NATIVE AUDIO) Volume changed.";
     [self.commandDelegate runInBackground:^{
         if (existingReference == nil) {
 			NSArray *pathArray = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask,YES);
-			/*NSString *documentsDirectory = [pathArray objectAtIndex:0];
+			NSString *documentsDirectory = [pathArray objectAtIndex:0];
 			NSString *path = [documentsDirectory stringByAppendingPathComponent:filename;
-
+			/*
 			if ([[NSFileManager defaultManager] fileExistsAtPath:path]) {
 			
                 NativeAudioAsset* asset = [[NativeAudioAsset alloc] initWithPath:path
