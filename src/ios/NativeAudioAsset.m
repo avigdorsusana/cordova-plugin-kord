23ef068d724d6aa496a6da98d8e6b6da5a6c20fd
Avigdor Susana <avigdor@Avigdors-MacBook-Air-2.local>
2019-08-07 11:50:19 -0400

    tunning stuff

--- src/ios/NativeAudioAsset.m
+++ src/ios/NativeAudioAsset.m
@@ -114,7 +114,7 @@ static const CGFloat FADE_DELAY = 0.08;
         AVAudioPlayer * player = [voices objectAtIndex:x];
         double time = [player duration];
     }
-	return((double) 12);
+	return(time);
 }
 
 - (double) currentTime
