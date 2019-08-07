c9599b00f657b375eb42e43b0d265eba51f6cf9f
Avigdor Susana <avigdor@Avigdors-MacBook-Air-2.local>
2019-08-07 02:44:33 -0400

    tunning stuff

--- src/ios/NativeAudio.m
+++ src/ios/NativeAudio.m
@@ -486,7 +486,7 @@ NSString* INFO_VOLUME_CURRENTTIME = @"(NATIVE AUDIO) Current Time.";
         if (asset != nil){
 
             NativeAudioAsset *_asset = (NativeAudioAsset*) asset;
-			double *time = [_asset duration];
+			double time = [_asset duration];
 			
 			NSString *RESULT = [NSString stringWithFormat:@"%@ (%@)", INFO_VOLUME_CURRENTTIME, time];
 			[self.commandDelegate sendPluginResult:[CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString: RESULT] callbackId:callbackId];
