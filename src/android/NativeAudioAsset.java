15549b663b82baa2572e884a062bf2cd4de109e4
Chuuken <fmendez133@gmail.com>
2019-08-14 14:17:00 -0400

    Add cyclic barrier for synchronized start

--- src/android/NativeAudioAsset.java
+++ src/android/NativeAudioAsset.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.concurrent.Callable;
 import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.BrokenBarrierException;
 
 import android.content.Context;
 import android.content.res.AssetFileDescriptor;
