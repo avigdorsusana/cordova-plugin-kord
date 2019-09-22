1bcb90e75d28444703bcef034b03bb643f5ba1a8
Chuuken <fmendez133@gmail.com>
2019-09-22 16:02:16 -0400

    Add cast to NativeAudioAsset in schedule method for timer.

--- src/android/NativeAudio.java
+++ src/android/NativeAudio.java
@@ -409,7 +409,7 @@ public class NativeAudio extends CordovaPlugin implements AudioManager.OnAudioFo
 							});
 
 						playTimer.schedule(
-							asset,
+							(TimerTask) asset,
 							playTime
 						);
 						
