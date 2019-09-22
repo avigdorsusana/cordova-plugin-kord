3c3f7a7ec987377ee566c64e647b03d83b0129b7
Chuuken <fmendez133@gmail.com>
2019-09-22 15:49:42 -0400

    Added store callback function to NativeAudioAsset. Removed callable from constructor. Now stores the callback before calling run method.

--- src/android/NativeAudioAsset.java
+++ src/android/NativeAudioAsset.java
@@ -8,27 +8,13 @@ import java.util.concurrent.Callable;
 import android.content.Context;
 import android.content.res.AssetFileDescriptor;
 
-import org.json.JSONObject;
-
 public class NativeAudioAsset extends TimerTask
 {
 
 	private ArrayList<NativeAudioAssetComplex> voices;
 	private int playIndex = 0;
-	
-	private Callable<Void> completeCb = new Callable<Void>(){
-		public Void call() throws Exception {
-			if (completeCallbacks != null) {
-				CallbackContext callbackContext = completeCallbacks.get(key);
-				if (callbackContext != null) {
-				JSONObject done = new JSONObject();
-				done.put("id", key);
-				// callbackContext.sendPluginResult(new PluginResult(Status.OK, done));
-				}
-			}
-			return null;
-		}
-	};
+
+	private Callable<Void> completeCb;
 	
 	public NativeAudioAsset(AssetFileDescriptor afd, int numVoices, float volume) throws IOException
 	{
@@ -191,6 +177,10 @@ public class NativeAudioAsset extends TimerTask
 			NativeAudioAssetComplex voice = voices.get(x);
 			voice.setVolume(volume);
 		}
+	}
+
+	public void storeCallback(Callable<Void> completeCb){
+		this.completeCb = completeCb;
 	}
 
 	public void run(){
